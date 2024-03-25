2607b32da2c9755ea36c268120c9e2c8648bc9e9
Wardi Soulaimana <wardi@wardi-s.com>
2024-03-25 04:00:35 +0000

    add pokemon color

--- app/[pokemonName]/page.tsx
+++ app/[pokemonName]/page.tsx
@@ -6,6 +6,36 @@ import { getPokemonByName } from "@/lib/pokemonAPI";
 import { LiaRulerVerticalSolid } from "react-icons/lia";
 import { TbWeight } from "react-icons/tb";
 
+const TYPE_COLOR_MAP = {
+  normal: "#A8A878",
+  fire: "#F08030",
+  water: "#6890F0",
+  electric: "#F8D030",
+  grass: "#78C850",
+  ice: "#98D8D8",
+  fighting: "#C03028",
+  poison: "#A040A0",
+  ground: "#E0C068",
+  flying: "#A890F0",
+  psychic: "#F85888",
+  bug: "#A8B820",
+  rock: "#B8A038",
+  ghost: "#705898",
+  dragon: "#7038F8",
+  dark: "#705848",
+  steel: "#B8B8D0",
+  fairy: "#EE99AC",
+};
+
+const STAT_NAME_MAP = {
+  hp: "HP",
+  attack: "ATK",
+  defense: "DEF",
+  "special-attack": "SATK",
+  "special-defense": "SDEF",
+  speed: "SPD",
+};
+
 export default async function PokemonPage({
   params,
 }: {
@@ -13,21 +43,18 @@ export default async function PokemonPage({
 }) {
   const { pokemonName } = params;
   const pokemon = await getPokemonByName(pokemonName);
-
-  const statNameMap = {
-    hp: "HP",
-    attack: "ATK",
-    defense: "DEF",
-    "special-attack": "SATK",
-    "special-defense": "SDEF",
-    speed: "SPD",
-  };
+  const mainType = pokemon.types[0].type.name;
+  const color = TYPE_COLOR_MAP[mainType as keyof typeof TYPE_COLOR_MAP];
 
   return (
-    <div>
-      <Card className="mx-auto border-4">
-        <CardHeader>
-          <CardTitle className="pb-4 text-center capitalize">
+    <div className="py-4">
+      <Card className="mx-auto border-4" style={{ borderColor: color }}>
+        <CardHeader className="relative">
+          <div
+            className={`absolute left-0 top-0 z-0 w-full py-32`}
+            style={{ backgroundColor: color }}
+          ></div>
+          <CardTitle className="z-10 pb-4 text-center capitalize">
             {pokemon.name}
           </CardTitle>
           <div className="relative grid min-h-60 w-full place-items-center">
@@ -45,6 +72,12 @@ export default async function PokemonPage({
                 <Badge
                   key={type.type.name}
                   className="px-4 py-2 font-bold capitalize"
+                  style={{
+                    backgroundColor:
+                      TYPE_COLOR_MAP[
+                        type.type.name as keyof typeof TYPE_COLOR_MAP
+                      ],
+                  }}
                 >
                   {type.type.name}
                 </Badge>
@@ -87,7 +120,11 @@ export default async function PokemonPage({
                   key={stat.stat.name}
                 >
                   <p>
-                    {statNameMap[stat.stat.name as keyof typeof statNameMap]}
+                    {
+                      STAT_NAME_MAP[
+                        stat.stat.name as keyof typeof STAT_NAME_MAP
+                      ]
+                    }
                   </p>
                   <p>{stat.base_stat}</p>
                   <Progress value={stat["base_stat"]} className="h-2" />
